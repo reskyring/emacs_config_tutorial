765567c13e9838381327a8b198d17ffb042e4554
skyring <reskyring@mail.com>
2019-06-29 12:26:10 +0800

    restore bug

--- init.el
+++ init.el
@@ -5,10 +5,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org"   . "https://orgmode.org/elpa/")
-			 ("ELPA"  . "https://tromey.com/elpa/")
-			 ("gnu"   . "https://elpa.gnu.org/packages/")
-			 ("SC" . "https://joseito.republika.pl/sunrise-commander/")))
+			 ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -40,19 +37,3 @@
 ;;(setq auto-save-default nil)
 
 
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (git-timemachine git-gutter magit company-jedi elpy which-key use-package tabbar org-bullets ido-vertical-mode flycheck counsel company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
