918d3891f7a8acafc1210bd79a3aef132d6bd8bc
skyring <reskyring@mail.com>
2019-07-05 14:53:01 +0800

    init.el auto init

--- init.el
+++ init.el
@@ -37,3 +37,17 @@
 ;;(setq auto-save-default nil)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (which-key use-package tabbar org-bullets magit ido-vertical-mode git-timemachine git-gutter flycheck elpy counsel company-jedi))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
